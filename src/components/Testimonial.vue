1678c9c8ab30478659aebce5881a22547843c145
tgiannotta <t.giannotta92@gmail.com>
2022-01-19 18:05:59 +0100

    end testimonial

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -5,19 +5,54 @@
                 <h5>Testimonial</h5>
                 <h3>What people Say</h3>
                 <p>When, while the lovely valley teems with vapour around>meand the meridian sun surface</p>
-            </div>
+        </div>
         <div class="people-cards">
                 <div class="single-people">
                     <img src="../assets/img/fabio.png" alt="fabio">
+                        <div class="elementi">
+                            <h3>3 STELLE SU 5</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <h3>Michael Angel</h3>
+                            <p>Michael Angel</p>
+                        </div>
                 </div>
                 <div class="single-people">
-                                     <img src="../assets/img/fabio.png" alt="fabio">
+                                     <img src="../assets/img/filip.png" alt="fabio">
+                                      <div class="elementi">
+                            <h3>3 STELLE SU 5</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <h3>Michael Angel</h3>
+                            <p>Michael Angel</p>
+                        </div>
 
                 </div>
                 <div class="single-people">
-                                <img src="../assets/img/fabio.png" alt="fabio">
+                                <img src="../assets/img/viktor.png" alt="fabio">
+                                 <div class="elementi">
+                            <h3>3 STELLE SU 5</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <h3>Michael Angel</h3>
+                            <p>Michael Angel</p>
+                        </div>
 
                 </div>
+        </div>
+        <div class="pubblicita-container">
+            <div class="icone-pubblicita">
+                <img src="../assets/img/client1.png" alt="1">                
+            </div>
+             <div class="icone-pubblicita">
+                <img src="../assets/img/client2.png" alt="2">                
+            </div>
+             <div class="icone-pubblicita">
+                <img src="../assets/img/client3.png" alt="3">                
+            </div>
+             <div class="icone-pubblicita">
+                <img src="../assets/img/client4.png" alt="4">                
+            </div>
+             <div class="icone-pubblicita">
+                <img src="../assets/img/client5.png" alt="5">                
+            </div>
         </div>
 </div>
     
@@ -54,10 +89,10 @@ export default {
     display: flex;
     width: 80%;
     margin: auto;
+    padding-bottom: 400px;
     .single-people{
         width: calc(100% / 3);
         height: 150px;
-    border: 1px solid black;
     position: relative;
 
         img{
@@ -69,7 +104,32 @@ export default {
             top: 0px;
             left: 180px;
         }
+        .elementi{
+            position: absolute;
+            top: 170px;
+            left: 185px;
+            h3{
+                font-size: 20px;
+                padding: 20px 0px;
+            }
+            p{
+                font-size: 18px;
+            }
+        }
     }
 
+}
+.pubblicita-container{
+    width: 70%;
+    margin:auto;
+    display: flex;
+    .icone-pubblicita{
+        width: calc(100% / 5);
+        img{
+            width: 150px;
+            height: 100px;
+        }
+        
+    }
 }
 </style>
